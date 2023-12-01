63f9ce52c5b37b4f3923bb65a5ecb09170a227a0
Zahir Exequiel Cheveste <zcheveste@munipilar.com.ar>
2023-12-01 16:59:43 -0300

    app escritorio ejecutar scripts

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask, render_template, request, jsonify
 import subprocess
 import threading
-import signal
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 cancelar_ejecucion = False
-proceso_ejecucion = None
-procesos_ejecucion = []  # Declarar la variable
+procesos_ejecucion = []  # Lista para almacenar los hilos de ejecución
 
 def ejecutar_archivo(archivo_path, resultado):
-    global cancelar_ejecucion, proceso_ejecucion, procesos_ejecucion  # Corregir el nombre de la variable
+    global cancelar_ejecucion
 
     try:
         # Inicia un subproceso para ejecutar el archivo
-        proceso_ejecucion = subprocess.Popen(['python', archivo_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        proceso = subprocess.Popen(['python', archivo_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
         # Espera hasta que el archivo termine o se indique cancelar
-        while not cancelar_ejecucion and proceso_ejecucion.poll() is None:
+        while not cancelar_ejecucion and proceso.poll() is None:
             pass
 
     except subprocess.CalledProcessError as e:
@@ -24,29 +23,31 @@ def ejecutar_archivo(archivo_path, resultado):
     except Exception as e:
         resultado.append({"tipo": "Error", "contenido": f"Error inesperado al ejecutar el archivo: {str(e)}"})
 
-    # Reinicia las variables globales para la próxima ejecución
+    # Reinicia la bandera de cancelación
     cancelar_ejecucion = False
-    proceso_ejecucion = None
 
 def ejecutar_archivo_en_hilo(archivo_path, resultado):
-    global procesos_ejecucion  # Corregir el nombre de la variable
     hilo_ejecucion = threading.Thread(target=ejecutar_archivo, args=(archivo_path, resultado))
     hilo_ejecucion.start()
     procesos_ejecucion.append(hilo_ejecucion)
 
-@app.route('/execute', methods=['POST'])
+
 def execute_script():
     global cancelar_ejecucion, procesos_ejecucion
 
     script_name = request.form.get('nombre_script')
     
-    # Mapea el nombre del script al archivo correspondiente
+    import os
+
+    base_path = os.path.dirname(os.path.abspath(__file__))
+
     script_path_mapping = {
-        'ejecutar1': 'C:\\Users\\zcheveste\\Documents\\Robocop_project\\Ejecucion\\ejecutar1.py',
-        'ejecutar2': 'C:\\Users\\zcheveste\\Documents\\Robocop_project\\Ejecucion\\ejecutar2.py',
-        'robot1': 'C:\\Users\\zcheveste\\Documents\\Robocop_project\\Robot_CompromisoAysa\\tasks.robot',
-        'robot2': 'C:\\Users\\zcheveste\\Documents\\Robocop_project\\Robot_Comprobante\\tasks.robot',
+        'ejecutar1': os.path.join(base_path, 'Ejecucion', 'ejecutar1.py'),
+        'ejecutar2': os.path.join(base_path, 'Ejecucion', 'ejecutar2.py'),
+        'robot1': os.path.join(base_path, 'Robot_CompromisoAysa', 'tasks.robot'),
+        'robot2': os.path.join(base_path, 'Robot_Comprobante', 'tasks.robot'),
     }
+
 
     if script_name not in script_path_mapping:
         return jsonify(result="Script no válido")
@@ -64,7 +65,7 @@ def execute_script():
 
     return jsonify(result="Ejecutando archivo...", resultado=resultado)
 
-@app.route('/cancelar', methods=['POST'])
+
 def cancelar_ejecucion_script():
     global cancelar_ejecucion, procesos_ejecucion
     cancelar_ejecucion = True
@@ -75,7 +76,8 @@ def cancelar_ejecucion_script():
 
     return jsonify(result="Cancelando ejecución del archivo...")
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
